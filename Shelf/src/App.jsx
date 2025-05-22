926e3f0748048e70cd6230ba0e5abe8a54779f7f
Saviho3 <saviodoraiswamy@gmail.com>
2025-05-22 11:33:41 -0600

    React set up

---
+++ Shelf/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My App</h1>
+      {/* You can add routing or more components here */}
+    </div>
+  )
+}
+
+export default App
